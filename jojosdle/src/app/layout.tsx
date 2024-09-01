9bb3062f1e1d0675f54cb834defbe5a2136470d3
Marvinrn <marvinnrn@gmail.com>
2024-09-01 05:04:44 +0200

    fix: made a change to the favicon

--- jojosdle/src/app/layout.tsx
+++ jojosdle/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./JojoTitleIcon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
